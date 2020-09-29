4ec241258ff741b1ee1f4c002cb187ea7c3a0046
KAUSHAL <kpandey1532@conestogac.on.ca>
2020-09-29 17:15:26 -0400

    Basic display changes

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Student Success</Text>
       <StatusBar style="auto" />
     </View>
   );
