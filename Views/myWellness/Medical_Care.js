3a0f60244aca41d85d2bee4e70e73d0e1d7eb718
Presha <preshad99@gmail.com>
2020-10-29 10:29:29 -0400

    mentalhealth,medicalcare

--- Views/myWellness/Medical_Care.js
+++ Views/myWellness/Medical_Care.js
@@ -1,25 +1,159 @@
 import React, { Component } from 'react';
-import { Animated,KeyboardAvoidingView, StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Button, TouchableWithoutFeedback, Keyboard, Linking} from 'react-native';
+import { Animated, KeyboardAvoidingView, StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Button, TouchableWithoutFeedback, Keyboard, Linking } from 'react-native';
 import styles from '../../style';
-const mailSubject = 'Mobile Solution Development!!!'
-class MedicalCare extends Component {
-    render(){
+import style2 from '../../CSS/MyWellnessStyle';
+import CardView from 'react-native-cardview';
+import FlipCard from 'react-native-flip-card';
+import { Card } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
+import Unorderedlist from 'react-native-unordered-list';
+const mailSubject = 'Mobile Solution Development!!!';
+import { Link, NavigationContainer } from '@react-navigation/native';
+import HealthInsurance from './HealthInsurance'
+class MedicalCare extends Component{
+  render() {
     return (
-      
-              <View style={styles.container}>
+      <View style={style2.container}>
+      <Text style={[style2.titleText, { paddingBottom: 20 }]}>Medical Care</Text>
+      <View style={{width:"100%", marginBottom:20}}>
+          <Text style={{textAlign:"center", color:"grey"}}>Click on card to view description</Text>
+        </View>
+        <View style={style2.outer}>
+          <FlipCard style={[style2.flipcard,{paddingBottom:20}]}
+            flipVertical={false}
+            flipHorizontal={true}
+            friction={20}
+          >
 
-                  <Text style={styles.titleText}>MedicalCare</Text>
-                  <Text style={styles.paraText}>
-                        <Text style={styles.initial}>S</Text>
-                        <Text> 
-                            tudent Success Advisors are here to offer academic advice, help navigate college services and find the supports and tools that will work best for you. They can help book appointments with wellness and learning services, and can assist you in working through college processes or problems you might be experiencing. Asking for support is easy – just use the online self-referral form and an advisor will reach out to you and set-up a time to talk
-                        </Text>
-                  </Text>
-                  <Button style={styles.appButtonContainer} onPress={() => Linking.openURL('mailto:studentsuccess@conestogac.on.ca?subject={mailSubject}') }
-                    title="Reach Your Advisor" />
-              </View>  
+<View style={{ width: "100%" ,alignItems: 'center'  }}>
+<View style={ medicalCareCSS.CardView }>
+            <Text style={[style2.paraText,medicalCareCSS.CardViewText]}>Medical Services Available</Text>
+              
+            </View>
+
+              <View style= {medicalCareCSS.border}>
+              <Image style={medicalCareCSS.image} source={require('../../assets/res/medical.png')}></Image>
+              <View style= {{borderTopColor:"black" ,borderTopWidth:2 , width:"100%" ,display:"flex",flexDirection:"column",alignItems:"center" }}>
+                <Text>
+                <Text style={[style2.paraText,{marginLeft:0,marginTop:5}]}>Doctor and nurse practitioner appointments are available to all Conestoga College students with OHIP or other</Text><Text style={{color:"blue"}}><Link to='/HealthInsurance'> health insurance plans.</Link>  </Text>         
+                </Text>
+              </View>
+              </View>
+            </View>
+            <View style={medicalCareCSS.viewlist}>
+            <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}><Text style={style2.paraText}>Diagnosis and treatment of illness and injury</Text></Unorderedlist>
+                
+                <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Sexual health - STI testing, PAP smear, birth control, pregnancy testing, etc. 
+              </Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Prenatal care (up to 28 weeks)</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Specialist referrals</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Mental health support</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Sick notes and third-party forms ($)</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Skin treatments ($ for some)</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Immunizations and injections($)</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Lab and phlebotomy (blood) services (requisition required)</Text></Unorderedlist>
+             
+            </View>
+          </FlipCard>
+        </View>
+        <View style={{marginBottom:10}}></View>
+        <View style={style2.outer}>
+          <FlipCard style={[style2.flipcard,{paddingBottom:20}]}
+            flipVertical={false}
+            flipHorizontal={true}
+            friction={20}
+          >
+
+<View style={{ width: "100%" ,alignItems: 'center'  }}>
+<View style={ medicalCareCSS.CardView }>
+            <Text style={[style2.paraText,medicalCareCSS.CardViewText]}>What You need to Book an Appointment?</Text>
+            </View>
+              <View style= {medicalCareCSS.border}>
+              <Image style={medicalCareCSS.image1} source={require('../../assets/res/appointment.png')}></Image>
+              <View style= {{borderTopColor:"black" ,borderTopWidth:2 , width:"100%" ,display:"flex",alignItems:"center",margin:10 }}>
+                <Text>
+                <Text style={[style2.paraText,{marginLeft:0,marginTop:5}]}>Please visit our Privacy and Confidentiality page for details on our commitment to patient information security.</Text>      
+                </Text>
+              </View>
+              </View>
+            </View>
+            <View style={medicalCareCSS.viewlist}>
+            
+             <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              
+              <Text style={style2.paraText}>
+                <Text style={style2.paraText}>Students must present a valid </Text>
+                <Text style={[style2.paraText],{color:'blue'}} onPress={()=>{Linking.openURL('https://www.conestogac.on.ca/onecard/');}}>ONECard(Student ID)</Text>
+                <Text style={style2.paraText}>  and proof of <Text style={{color:"blue"}}><Link to='/HealthInsurance'> health insurance.</Link>  </Text>         
+                </Text> 
+                </Text>
+              
+             
+            </Unorderedlist>
+                
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+    
+              <Text style={style2.paraText}>Please arrive 5 minutes early to complete any required forms and questionnaires. 
+              </Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>Inform the front desk of any changes to your personal information.</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>There is a $30 fee for missed appointments without 24 hours' notice.</Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={medicalCareCSS.orderlist}>
+              <Text style={style2.paraText}>If you are late for an appointment, it will be at the medical team's discretion if they can still fit you in that day.</Text></Unorderedlist>
+              
+            </View>
+          </FlipCard>
+
+        </View>
+      </View>
     );
-    }
   }
-  
-  export default MedicalCare;
+}
+export default MedicalCare;
+
+const medicalCareCSS = StyleSheet.create({
+  CardView:
+  {
+    width: "100%",alignItems: 'center',display:'flex',justifyContent:'center',alignItems:'center'
+  },
+  CardViewText:
+  {textAlign:'justify',textAlign:'center',fontWeight:'bold',fontSize:24,alignItems:'center',display:'flex',justifyContent:'center'},
+  border:
+  {
+    borderTopColor:"black" ,borderTopWidth:2 , width:"90%" ,display:"flex",flexDirection:"column",alignItems:"center"
+  },
+  orderlist:
+  {
+    marginLeft:5
+  },
+  viewlist:{ height: 450, width: "100%", marginTop:20 },
+  image:{
+    width:200,
+    height:200,
+    marginLeft:2,
+    marginRight:2,
+   alignContent:'center',
+   alignItems:'center',
+    resizeMode:'contain',
+    flexWrap:'wrap'
+},
+image1:{
+  width:200,
+  height:"60%",
+  marginLeft:2,
+  marginRight:2,
+ alignContent:'center',
+ alignItems:'center',
+  resizeMode:'contain',
+  flexWrap:'wrap'
+}
+})
--- Views/myWellness/MentalHealth.js
+++ Views/myWellness/MentalHealth.js
@@ -5,12 +5,13 @@ import style2 from '../../CSS/MyWellnessStyle';
 import CardView from 'react-native-cardview';
 import FlipCard from 'react-native-flip-card';
 import { Card } from 'react-native-elements';
-
-const mailSubject = 'Mobile Solution Development!!!'
+import { ScrollView } from 'react-native-gesture-handler';
+import Unorderedlist from 'react-native-unordered-list';
+const mailSubject = 'Mobile Solution Development!!!';
 class MentalHealth extends Component {
   render() {
     return (
-
+      <ScrollView>
       <View style={style2.container}>
         <Text style={[style2.titleText, { paddingBottom: 20 }]}>MentalHealth Support and Resources</Text>
         <View style={{width:"100%", marginBottom:20}}>
@@ -46,6 +47,74 @@ class MentalHealth extends Component {
 
               </Text>
             </View>
+          </FlipCard>
+        </View>
+        <View style={{ marginTop: 10 }}></View>
+
+        <View style={style2.outer}>
+          <View style={{ borderBottomColor: "black", borderBottomWidth: 2, paddingBottom: 10, marginLeft: 20, marginRight: 20 }} >
+            <Text style={style2.titleText1}>Self Help</Text>
+          </View>
+          
+          <FlipCard style={[style2.flipcard,{paddingBottom:20}]}
+            flipVertical={false}
+            flipHorizontal={true}
+            friction={20}
+          >
+
+            <View style={{ width: "100%" ,alignItems: 'center'  }}>
+              <Image style={style2.image} source={require('../../assets/res/selfhelp.jpg')}></Image>
+              <View style= {{borderTopColor:"black" ,borderTopWidth:2 , width:"100%" ,display:"flex",flexDirection:"column",alignItems:"center" }}>
+
+
+                <Text style={[style2.paraText,{marginLeft:0,textAlign:"center" ,fontWeight: "bold", marginTop:10, color:"green" }]}>For more self-help ideas</Text>
+                <Text style={[style2.paraText,{marginLeft:0}]}> visit our mental health resources page. </Text>
+
+              </View>
+            </View>
+            <View style={{ height: 200, width: "100%", marginTop:20 }}>
+              <Text style={style2.paraText}>
+                <Text style={style2.initial}>S</Text>
+                <Text>
+                elf-help refers to techniques students can employ to maintain their own wellness. Another way of framing this is self-care. The Breathing Space is a great space to practice self-help and our peer support navigator is here to help provide guidance. 
+                        </Text>
+
+              </Text>
+            </View>
+          </FlipCard>
+         
+        </View>
+        <View style={{ marginTop: 10 }}></View>
+        <View style={style2.outer}>
+          <View style={{ borderBottomColor: "black", borderBottomWidth: 2, paddingBottom: 10, marginLeft: 20, marginRight: 20 }} >
+            <Text style={style2.titleText1}>Peer Support</Text>
+          </View>
+          
+          <FlipCard style={[style2.flipcard,{paddingBottom:20}]}
+            flipVertical={false}
+            flipHorizontal={true}
+            friction={20}
+          >
+
+            <View style={{ width: "100%" ,alignItems: 'center'  }}>
+              <Text style={[style2.paraText,{textAlign:'center',fontWeight:'bold',fontSize:24,padding:80,alignContent:'center'}]}>What is Peer Support?</Text>
+              <View style= {{borderTopColor:"black" ,borderTopWidth:2 , width:"100%" ,display:"flex",flexDirection:"column",alignItems:"center" }}>
+
+
+                <Text style={[style2.paraText,{marginLeft:0,textAlign:"center" ,fontWeight: "bold", marginTop:10, color:"green" }]}> Opening hours</Text>
+                <Text style={[style2.paraText,{marginLeft:0}]}> Monday - Friday from 8:30AM - 4PM</Text>
+
+              </View>
+            </View>
+            <View style={{ height: 200, width: "100%", marginTop:20 }}>
+              <Text style={style2.paraText}>
+                <Text style={style2.initial}>P</Text>
+                <Text>
+                eer support is a mental health and substance use service provided by an individual who has lived experience with these subjects. It focuses on building connections and sharing wellness tools and strategies. It is generally more relaxed and socially focused than counselling services, and can occur one-on-one or in a group setting.
+                        </Text>
+
+              </Text>
+            </View>
 
 
 
@@ -53,37 +122,79 @@ class MentalHealth extends Component {
 
         </View>
         <View style={{ marginTop: 10 }}></View>
-        <CardView
-          cardElevation={2}
-          cardMaxElevation={2}
-          style={{
-            marginLeft: 20, marginRight: 20, backgroundColor: "#eff0f1",
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 5,
-              height: 5,
-            },
-            shadowOpacity: 0.5,
-            shadowRadius: 3.84,
-            elevation: 5,
-          }}
-        >
-          <Text style={style2.titleText1}>Self Help</Text>
-          <View style={{ flexDirection: 'row-reverse' }}>
-            <Image style={style2.image} source={require('../../assets/res/selfhelp.jpg')}></Image>
-            <Text style={style2.paraText}>
-              <Text style={style2.initial}>T</Text>
-              <Text>
-                his student wellness space, located in Doon 1A107, is home to the Peer Support Navigator. It is a quiet place on campus for students to take a break. The room is stocked with stress-busting activities and runs wellness-focused, drop-in groups throughout the week.
-                        </Text>
+        <View style={style2.outer}>
+          
+            
+          
+          <FlipCard style={[style2.flipcard,{paddingBottom:20}]}
+            flipVertical={false}
+            flipHorizontal={true}
+            friction={20}
+          >
 
-            </Text>
-          </View>
-          <Text style={[style2.paraText, { width: '98%', marginTop: 3 }]}>
-            <Text>The Breathing Space is open Monday - Friday from 8:30 am - 4 pm.</Text>
-          </Text>
-        </CardView>
+            <View style={{ width: "100%", height:"100%",alignItems: 'center',display:'flex',justifyContent:'center',alignItems:'center'  }}>
+            <Text style={[style2.paraText,{textAlign:'center',fontWeight:'bold',fontSize:24,alignContent:'Justify',alignItems:'center'}]}>Help A Student</Text>
+              
+            </View>
+            <View style={{ height: 450, width: "100%", marginTop:20 }}>
+              <Text style={style2.paraText}>
+                <Text style={style2.initial}>A</Text>
+                <Text>
+                re you worried about a friend's well-being? Often asking them if they need someone to talk to, and taking the time to listen, is a great first step to providing them with help. We suggest connecting with the Peer Support Navigator for advice or encouraging your friend to connect with one of the college wellness services.
+                </Text>
+                </Text>
+                <Text style={style2.paraText}>
+              <Text>If you are concerned your friend is in immediate danger of harming themselves or others, please refer to our Emergency and Crisis Care information. 
+                 </Text>
+
+              </Text>
+            </View>
+
+
+
+          </FlipCard>
+
+        </View>
+        <View style={{ marginTop: 10 }}></View>
+        <View style={{ marginTop: 10 }}></View>
+        <View style={style2.outer}>
+          
+            
+          
+          <FlipCard style={[style2.flipcard,{paddingBottom:20}]}
+            flipVertical={false}
+            flipHorizontal={true}
+            friction={20}
+          >
+
+            <View style={{ width: "100%", height:"100%",alignItems: 'center',display:'flex',justifyContent:'center',alignItems:'center'  }}>
+            <Text style={[style2.paraText,{textAlign:'center',fontWeight:'bold',fontSize:24,alignContent:'Justify',alignItems:'center'}]}>Off-campus mental health supports for students</Text>
+              
+            </View>
+            <View style={{ height: 450, width: "100%", marginTop:20 }}>    
+                <Unorderedlist bulletUnicode={0x2022} style={{marginLeft:5}}>
+                  <Text style={style2.paraText}>
+                   <Text style={[style2.paraText],{color:'blue'}} onPress={()=>{Linking.openURL('https://www.conestogac.on.ca/onecard/');}}>Good2Talk</Text>
+                   <Text> provides free and confidential support services for all Ontario post-secondary students, 24 hours a day, 7 days a week, 365 days a year.</Text>
+                  </Text>
+                </Unorderedlist>
+                
+                <Unorderedlist bulletUnicode={0x2022} style={{marginLeft:5}}>
+              <Text style={style2.paraText}>If you are concerned your friend is in immediate danger of harming themselves or others, please refer to our Emergency and Crisis Care information. 
+              </Text></Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2022} style={{marginLeft:5}}>
+              <Text style={style2.paraText}>
+                   <Text style={[style2.paraText],{color:'blue'}} onPress={()=>{Linking.openURL('https://www.conestogac.on.ca/onecard/');}}>Here247</Text>
+              <Text style={style2.paraText}> is an addictions, mental health and crisis services hotline for individuals in Waterloo and Wellington regions. They help navigate community mental health, recovery and medical services. 
+              </Text></Unorderedlist>
+
+             
+            </View>
+          </FlipCard>
+        </View>
+        <View style={{ marginBottom: 20 }}></View>
       </View>
+      </ScrollView>
     );
   }
 }
