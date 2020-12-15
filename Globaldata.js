99def65d8d7ea217e9eb1f04ca03ca462d263a17
KAUSHAL <kaushalpandey1510@gmail.com>
2020-12-14 23:30:42 -0500

    final commit

--- Globaldata.js
+++ Globaldata.js
@@ -1,6 +1,6 @@
 
 
-export var Server_Url = "http://7ac709dff9d1.ngrok.io";
+export var Server_Url = "http://aadfd73a13ff.ngrok.io";
 export var Default_Image_Url = "https://t3.ftcdn.net/jpg/02/70/09/98/360_F_270099822_9zbx236dHn1hyxYNl9HSOBvpUEpU0eOz.jpg";
 var notiId = -1;
 export function setNotiId(id){
